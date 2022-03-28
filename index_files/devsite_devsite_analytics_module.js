dddbc1f40db3ad7e7a58500457bd470e264903b7
Dil <58573780+mail2diku@users.noreply.github.com>
2022-03-28 11:23:08 -0400

    Initial commit

--- index_files/devsite_devsite_analytics_module.js
+++
@@ -1,1 +0,0 @@
-(function(_ds){var window=this;'use strict';try{window.customElements.define("devsite-analytics",_ds.$A)}catch(a){console.warn("devsite.app.customElement.DevsiteAnalytics",a)};})(_ds_www);
--- index_files/devsite_devsite_header_module.js
+++
@@ -1,1 +0,0 @@
-(function(_ds){var window=this;'use strict';try{window.customElements.define("devsite-header",_ds.hI)}catch(a){console.warn("devsite.app.customElement.DevsiteHeader",a)};})(_ds_www);
